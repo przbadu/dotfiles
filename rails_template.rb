aab59a598fbb656216002826c779f5df1d9e2dd8
Pushpa Raj Badu <prz.badu@gmail.com>
2024-02-05 20:59:01 +0545

    initial commit

---
+++ rails_template.rb
@@ -0,0 +1,11 @@
+gem_group :development, :test do
+  gem 'active_record_doctor'
+  gem 'benchmark-ips'
+  gem 'benchmark-memory'
+  gem 'database_consistency'
+  gem 'dotenv-rails' # Manage .env
+  gem 'faker', require: false
+  gem 'rails_best_practices'
+  gem 'rails-erd'
+  gem 'rails-pg-extras'
+end
