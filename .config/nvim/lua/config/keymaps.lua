95c9cc46c4eb158509b6f3f11739ecad10507319
Pushpa Raj Badu <przbadu.social@gmail.com>
2024-07-02 14:49:01 +0545

    Fixed rails 6.0 puma crash because of postgresql PGGSSENCMODE

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+
+-- Paste over currently selected text without yanking it
+keymap("v", "p", '"_dP', default_opts)
+
+-- vim-test
+-- Define key mappings for test commands
+keymap("n", "<leader>tt", ":TestNearest<CR>", default_opts)
+keymap("n", "<leader>tf", ":TestFile<CR>", default_opts)
+keymap("n", "<leader>ts", ":TestSuite<CR>", default_opts)
+keymap("n", "<leader>tl", ":TestLast<CR>", default_opts)
+keymap("n", "<leader>tg", ":TestVisit<CR>", default_opts)
+
+-- autorun current ruby file
+keymap("n", "<leader>rr", ":term ruby % <CR>", default_opts)
---
+++ .config/nvim/lua/plugins/rails.lua
@@ -0,0 +1,8 @@
+-- Ruby on Rails IDE
+return {
+  { "tpope/vim-rails" },
+  { "tpope/vim-surround" },
+  { "suketa/nvim-dap-ruby" }, -- debugger support using nvim-dap
+  { "ngmy/vim-rubocop" }, -- rubocop for linting and formatting
+  { "metakirby5/codi.vim" }, -- print ruby code line by line
+}
