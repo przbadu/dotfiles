156b0895eb5ccd599114158d658eab4ceeb0e4aa
Pushpa Raj Badu <przbadu.social@gmail.com>
2024-08-23 17:22:06 +0545

    Add ruby, codium, markdown as supported extra packages for lazyvim

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,23 +1,8 @@
 return {
-  -- catppuccino theme
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  -- one dark theme
-  {
-    "navarasu/onedark.nvim",
-    name = "onedark",
-    priority = 1000,
-    opts = {
-      style = "darker",
-    },
-  },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "catppuccin-latte",
-      colorscheme = "catppuccin",
-      -- colorscheme = "onedark",
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
