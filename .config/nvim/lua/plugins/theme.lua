c19b514ea920713eeddfa4daf0e13f2d1c2c7e22
przbadu <przbadu.social@gmail.com>
2025-01-27 12:30:27 +0545

    Add kitty config

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,22 +1,12 @@
 return {
   -- catppuccino theme
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  -- one dark theme
-  {
-    "navarasu/onedark.nvim",
-    name = "onedark",
-    priority = 1000,
-    opts = {
-      style = "darker",
-    },
-  },
-
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-latte",
-      -- colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin-latte",
+      colorscheme = "catppuccin",
       -- colorscheme = "onedark",
     },
   },
