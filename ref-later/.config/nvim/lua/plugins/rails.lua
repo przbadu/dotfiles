a51a192688fb22ab67bdd5e0c3022424e943dbb2
przbadu <przbadu.social@gmail.com>
2025-06-11 11:53:00 +0545

    remove unwanted files

--- ref-later/.config/nvim/lua/plugins/rails.lua
+++
@@ -1,8 +0,0 @@
--- Ruby on Rails IDE
-return {
-  { "tpope/vim-rails" },
-  { "tpope/vim-surround" },
-  { "suketa/nvim-dap-ruby" }, -- debugger support using nvim-dap
-  -- { "ngmy/vim-rubocop" }, -- rubocop for linting and formatting
-  { "metakirby5/codi.vim" }, -- print ruby code line by line
-}
