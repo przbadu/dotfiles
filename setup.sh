5bbf106ed92366a4a358beaf1e2e0900a7ede69a
przbadu <przbadu.social@gmail.com>
2025-06-13 11:04:07 +0545

    Add missing packages

--- setup.sh
+++ setup.sh
@@ -1238,6 +1238,14 @@ install_custom_packages() {
       log "Installing heroku cli"
       curl https://cli-assets.heroku.com/install.sh | sh
     fi
+
+    log "Install stripe"
+    if ! command_exists stripe; then
+      curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | sudo tee /usr/share/keyrings/stripe.gpg
+      echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | sudo tee -a /etc/apt/sources.list.d/stripe.list
+      sudo apt update
+      sudo apt install stripe
+    fi
   fi
 }
 
